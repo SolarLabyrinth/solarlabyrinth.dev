7f2e3a727aaefcb6f9019e05e3d7cf16806764e0
SolarLabyrinth <thesolarlabyrinth@gmail.com>
2024-10-21 00:13:01 -0500

    update content

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,26 +1,104 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { MetaFunction } from "@remix-run/cloudflare";
-import {
-  faTwitch,
-  faItchIo,
-  faGithub,
-  faBluesky,
-  faXTwitter,
-  faDiscord,
-} from "@fortawesome/free-brands-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBluesky } from "@fortawesome/free-brands-svg-icons/faBluesky";
+import { faDiscord } from "@fortawesome/free-brands-svg-icons/faDiscord";
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+import { faItchIo } from "@fortawesome/free-brands-svg-icons/faItchIo";
+import { faTwitch } from "@fortawesome/free-brands-svg-icons/faTwitch";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons/faXTwitter";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons/faYoutube";
+
+import { faFlask } from "@fortawesome/free-solid-svg-icons/faFlask";
+import { faGhost } from "@fortawesome/free-solid-svg-icons/faGhost";
+import { faHatCowboy } from "@fortawesome/free-solid-svg-icons/faHatCowboy";
+
+import { cx } from "~/utils/cx";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+const title = "SolarLabyrinth";
+const description =
+  "Elden Lord that does software and game dev in their free time.";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "SolarLabyrinth" },
-    { name: "description", content: "Solar's Website" },
+    { title },
+    {
+      name: "description",
+      content: description,
+    },
   ];
 };
 
+type ListProps = {
+  items: {
+    icon: IconDefinition;
+    description: string;
+    title: string;
+    href?: string;
+  }[];
+};
+function List({ items }: ListProps) {
+  return (
+    <ul>
+      {items.map((profile) => {
+        const baseItemClassName = cx(
+          "flex",
+          "items-center",
+          "gap-2",
+          "p-2",
+          "rounded-lg"
+        );
+        const linkClassName = cx(
+          baseItemClassName,
+          "hover:bg-neutral-200",
+          "dark:hover:bg-neutral-700"
+        );
+
+        const contents = (
+          <>
+            {profile.icon && (
+              <FontAwesomeIcon
+                className="flex-initial"
+                fixedWidth
+                size="2x"
+                icon={profile.icon}
+              />
+            )}
+            <div className="flex-1">
+              {profile.title && (
+                <div className="text-xl font-bold text-neutral-800 dark:text-1b-white">
+                  {profile.title}
+                </div>
+              )}
+              {profile.description && (
+                <div className="text-md font-bold text-neutral-500 dark:text-neutral-400">
+                  {profile.description}
+                </div>
+              )}
+            </div>
+          </>
+        );
+        return (
+          <li key={profile.description}>
+            {profile.href ? (
+              <a href={profile.href} className={linkClassName}>
+                {contents}
+              </a>
+            ) : (
+              <div className={baseItemClassName}>{contents}</div>
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
 export default function Index() {
   return (
-    <div className="w-full max-w-[600px] m-auto h-screen border-neutral-300 dark:border-neutral-700 border-x-2">
+    <div className="w-full max-w-[600px] m-auto min-h-screen border-neutral-300 dark:border-neutral-700 border-x-2">
       <header className="px-8 pt-8 mb-8">
-        <img src="" alt="" />
         <div className="flex gap-4 items-center">
           <img
             className="size-24 rounded-full"
@@ -30,45 +108,22 @@ export default function Index() {
             height="512"
           />
           <div>
-            <h1 className="text-2xl font-bold">SolarLabyrinth</h1>
-            <p>
-              Elden Lord that does software and game dev in their free time.
+            <h1 className="text-2xl font-bold">{title}</h1>
+            <p className="text-md font-semibold text-neutral-500 dark:text-neutral-400">
+              {description}
             </p>
           </div>
         </div>
       </header>
-      <main className="px-4">
-        <h2 className="text-xl px-3 my-2 font-bold">Profiles</h2>
-        <ul>
-          {profiles.map((profile) => {
-            const className =
-              "flex items-center gap-2 p-2 hover:bg-neutral-200 dark:hover:bg-neutral-700 rounded-lg";
-            const contents = (
-              <>
-                <FontAwesomeIcon fixedWidth size="2x" icon={profile.icon} />
-                <div>
-                  <div className="text-md text-neutral-800 dark:text-1b-white">
-                    {profile.profileName}
-                  </div>
-                  <div className="text-sm text-neutral-500 dark:text-neutral-400">
-                    {profile.name}
-                  </div>
-                </div>
-              </>
-            );
-            return (
-              <li key={profile.name}>
-                {profile.link ? (
-                  <a href={profile.link} className={className}>
-                    {contents}
-                  </a>
-                ) : (
-                  <div className={className}>{contents}</div>
-                )}
-              </li>
-            );
-          })}
-        </ul>
+      <main>
+        <div className="px-4">
+          <h2 className="text-xl px-3 mt-4 mb-1 font-bold">Profiles</h2>
+          <List items={profiles} />
+        </div>
+        <div className="px-4">
+          <h2 className="text-xl px-3 mt-4 mb-1 font-bold">Games</h2>
+          <List items={games} />
+        </div>
       </main>
     </div>
   );
@@ -77,37 +132,67 @@ export default function Index() {
 const profiles = [
   {
     icon: faTwitch,
-    name: "Twitch",
-    profileName: "SolarLabyrinth",
-    link: "https://www.twitch.tv/solarlabyrinth",
+    title: "SolarLabyrinth",
+    description: "Twitch",
+    href: "https://www.twitch.tv/solarlabyrinth",
   },
   {
     icon: faItchIo,
-    name: "Itch",
-    profileName: "SolarLabyrinth",
-    link: "https://solarlabyrinth.itch.io/",
+    title: "SolarLabyrinth",
+    description: "Itch",
+    href: "https://solarlabyrinth.itch.io/",
   },
   {
     icon: faGithub,
-    name: "Github",
-    profileName: "SolarLabyrinth",
-    link: "https://github.com/SolarLabyrinth",
+    title: "SolarLabyrinth",
+    description: "Github",
+    href: "https://github.com/SolarLabyrinth",
   },
   {
     icon: faBluesky,
-    name: "Bluesky",
-    profileName: "@solarlabyrinth.dev",
-    link: "https://bsky.app/profile/solarlabyrinth.dev",
+    title: "@solarlabyrinth.dev",
+    description: "Bluesky",
+    href: "https://bsky.app/profile/solarlabyrinth.dev",
+  },
+  {
+    icon: faYoutube,
+    title: "@TheSolarLabyrinth",
+    description: "YouTube",
+    href: "https://www.youtube.com/@TheSolarLabyrinth",
   },
   {
     icon: faXTwitter,
-    name: "Twitter",
-    profileName: "@solarlabyrinth",
-    link: "https://x.com/solarlabyrinth",
+    title: "@solarlabyrinth",
+    description: "Twitter",
+    href: "https://x.com/solarlabyrinth",
   },
   {
     icon: faDiscord,
-    name: "Discord",
-    profileName: "solarlabyrinth",
+    title: "solarlabyrinth",
+    description: "Discord",
+  },
+];
+
+const games = [
+  {
+    icon: faFlask,
+    title: "Aqua Regia",
+    description:
+      "Combine elemental essences, purify your alchemical solution, and turn lead into gold in this physics-based puzzle game.",
+    href: "https://solarlabyrinth.itch.io/aqua-regia",
+  },
+  {
+    icon: faGhost,
+    title: "Lunariel",
+    description:
+      "Banish the shadow spirits and restore the light of Lunariel to this once-hallowed forest.",
+    href: "https://solarlabyrinth.itch.io/lunariel",
+  },
+  {
+    icon: faHatCowboy,
+    title: "Saloon King",
+    description:
+      "Voice Acted for Tom Milman's Jern Jam 3 Entry. A short western shooter about a cowboy who wants to take over a Saloon and its town",
+    href: "https://solarlabyrinth.itch.io/lunariel",
   },
 ];
