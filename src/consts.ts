3cd97bdbc0d4b421d4baa7f5b0a9eb0099d03148
SolarLabyrinth <thesolarlabyrinth@gmail.com>
2025-04-07 00:11:52 -0500

    socials

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,53 @@
 export const SITE_TITLE = "SolarLabyrinth";
 export const SITE_DESCRIPTION =
   "Elden Lord that does software and game dev in their free time.";
+
+export const socials = [
+  {
+    icon: "github",
+    title: "SolarLabyrinth",
+    description: "Github",
+    href: "https://github.com/SolarLabyrinth",
+  },
+  {
+    icon: "itch",
+    title: "SolarLabyrinth",
+    description: "Itch.io",
+    href: "https://solarlabyrinth.itch.io/",
+  },
+  {
+    icon: "twitch",
+    title: "SolarLabyrinth",
+    description: "Twitch",
+    href: "https://www.twitch.tv/solarlabyrinth",
+  },
+  {
+    icon: "bluesky",
+    title: "@solarlabyrinth.dev",
+    description: "Bluesky",
+    href: "https://bsky.app/profile/solarlabyrinth.dev",
+  },
+  {
+    icon: "discord",
+    title: "solarlabyrinth",
+    description: "Discord | Username",
+  },
+  {
+    icon: "youtube",
+    title: "@TheSolarLabyrinth",
+    description: "YouTube",
+    href: "https://www.youtube.com/@TheSolarLabyrinth",
+  },
+  {
+    icon: "mastodon",
+    title: "@SolarLabyrinth",
+    description: "Mastodon | @mastodon.social",
+    href: "https://mastodon.social/@SolarLabyrinth",
+  },
+  {
+    icon: "twitter",
+    title: "@solarlabyrinth",
+    description: "Twitter",
+    href: "https://x.com/solarlabyrinth",
+  },
+];
